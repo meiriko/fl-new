489e95fe258756b2a5b75a5aca3ef595ba67658d
Meir <meir@finaloop.com>
2024-09-05 15:44:41 +0300

    added component library

--- apps/routing-demo/src/App.tsx
+++ apps/routing-demo/src/App.tsx
@@ -1,33 +1,38 @@
 import { Link, Outlet } from "@tanstack/react-router";
+import { ChakraProvider } from "@chakra-ui/react";
+import { DemoButton } from "fl-components";
 
 function App() {
   return (
-    <div>
-      <div style={{ display: "flex", gap: "1rem", padding: 20 }}>
-        <Link to="/">Home</Link>
-        <Link to="/miro">Miro</Link>
-        <Link to="/miro/$miroId" params={{ miroId: "xxx" }}>
-          Miro xxx
-        </Link>
-        <Link to="/miro/$miroId" params={{ miroId: "yyy" }}>
-          Miro yyy
-        </Link>
-        <Link
-          to="/miro/$miroId/$tabId"
-          params={{ miroId: "yyy", tabId: "overview" }}
-        >
-          Miro yyy/overview
-        </Link>
-        <Link
-          to="/miro/$miroId/$tabId"
-          params={{ miroId: "yyy", tabId: "settings" }}
-        >
-          Miro yyy/settings
-        </Link>
+    <ChakraProvider>
+      <div>
+        <DemoButton />
+        <div style={{ display: "flex", gap: "1rem", padding: 20 }}>
+          <Link to="/">Home</Link>
+          <Link to="/miro">Miro</Link>
+          <Link to="/miro/$miroId" params={{ miroId: "xxx" }}>
+            Miro xxx
+          </Link>
+          <Link to="/miro/$miroId" params={{ miroId: "yyy" }}>
+            Miro yyy
+          </Link>
+          <Link
+            to="/miro/$miroId/$tabId"
+            params={{ miroId: "yyy", tabId: "overview" }}
+          >
+            Miro yyy/overview
+          </Link>
+          <Link
+            to="/miro/$miroId/$tabId"
+            params={{ miroId: "yyy", tabId: "settings" }}
+          >
+            Miro yyy/settings
+          </Link>
+        </div>
+        <div>root (App)</div>
+        <Outlet />
       </div>
-      <div>root (App)</div>
-      <Outlet />
-    </div>
+    </ChakraProvider>
   );
 }
 
---
+++ libs/fl-components/lib/components/DemoButton.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@chakra-ui/react";
+
+export function DemoButton() {
+  return <Button>Click me</Button>;
+}
