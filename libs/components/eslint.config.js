a447a5b636af921a3e41ebf754fdc28d1e69d421
Meir <meir@finaloop.com>
2024-09-08 14:50:38 +0300

    configured eslint for storybook @fl/* imports

--- libs/components/eslint.config.js
+++ libs/components/eslint.config.js
@@ -1,19 +1,28 @@
 import tseslint from "typescript-eslint";
 import baseConfig, { mergeImportRestriction } from "../../eslint.config.js";
 
-export default tseslint.config(...baseConfig, {
-  rules: {
-    "no-restricted-imports": [
-      "error",
-      mergeImportRestriction(baseConfig, {
-        patterns: [
-          {
-            group: ["@fl/*"],
-            message:
-              "@fl/components is a low level library, it should not import other @fl/* libraries",
-          },
-        ],
-      }),
-    ],
+export default tseslint.config(
+  ...baseConfig,
+  {
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        mergeImportRestriction(baseConfig, {
+          patterns: [
+            {
+              group: ["@fl/*"],
+              message:
+                "@fl/components is a low level library, it should not import other @fl/* libraries",
+            },
+          ],
+        }),
+      ],
+    },
   },
-});
+  {
+    files: ["**/{stories,.storybook}/**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "no-restricted-imports": "off",
+    },
+  }
+);
