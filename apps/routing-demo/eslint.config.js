001f714de490767a34ba8d54e607f563c20800a8
Meir <meir@finaloop.com>
2024-09-10 11:35:05 +0300

    improved apps linting

--- apps/routing-demo/eslint.config.js
+++ apps/routing-demo/eslint.config.js
@@ -1,5 +1,22 @@
-// import tseslint from "typescript-eslint";
+import tseslint from "typescript-eslint";
 import baseConfig from "../../eslint.config.js";
 
-// export default tseslint.config(...baseConfig);
-export default baseConfig;
+export default tseslint.config(
+  ...baseConfig,
+
+  {
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: ["libs/*"],
+              message: "Do not import from libs/* directly, use @fl/* instead",
+            },
+          ],
+        },
+      ],
+    },
+  }
+);
