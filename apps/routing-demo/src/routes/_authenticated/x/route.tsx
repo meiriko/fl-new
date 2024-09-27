fa325cf6bc6cbcf967ebeb236ca9a16c11fe9a3e
Meir <meir@finaloop.com>
2024-09-27 16:56:10 +0300

    initial gql svc + tanstack query integration

--- apps/routing-demo/src/routes/_authenticated/x/route.tsx
+++ apps/routing-demo/src/routes/_authenticated/x/route.tsx
@@ -1,10 +1,67 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { toService } from "@fl/dataApi";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+// import _ from "lodash";
 
 export const Route = createFileRoute("/_authenticated/x")({
-  component: () => (
+  component: XDisplay,
+});
+
+const vanillaChipCompanyId = "a85a932a-e826-424c-966e-6f5831ec47be";
+
+const svcBase = toService(
+  "onboardingStatus",
+  { status: true, scope: true, step: true },
+  { scope: "INVENTORY", companyId: vanillaChipCompanyId },
+);
+
+// const svc = _.ary(svcBase, 0);
+const svc = ({ queryKey }: { queryKey: string[] }) => {
+  const [_key, step] = queryKey;
+  console.log("fetch step: ", step);
+  return svcBase({ step });
+};
+
+const steps = [
+  "purchase",
+  "intro",
+  "setup",
+  "vendor-balance",
+  "recipes",
+  "review",
+  "unit-based-tracking-welcome",
+  "balance",
+  "warehouse-transfer",
+];
+
+function XDisplay() {
+  const [step, setStep] = useState(steps[0]);
+  const dbg = useQuery({
+    queryKey: ["dbg", step],
+    queryFn: svc,
+  });
+  useEffect(() => {
+    console.log(">>> dbg >>>>> : ", JSON.parse(JSON.stringify(dbg)));
+  }, [dbg]);
+
+  return (
     <div>
       <div>Hello /_authenticated/welcome/x!</div>
+      <div style={{ margin: "20px 5px" }}>
+        <div style={{ marginBottom: 10 }}>step: {step}</div>
+        <label>
+          steps:
+          <select onChange={(e) => setStep(e.target.value)}>
+            {steps.map((step) => (
+              <option value={step} key={step}>
+                {step}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
       <Outlet />
     </div>
-  ),
-});
+  );
+}
--- apps/routing-demo/src/routes/_authenticated/x/y/route.tsx
+++ apps/routing-demo/src/routes/_authenticated/x/y/route.tsx
@@ -1,7 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { asTypedParams } from "@fl/routing";
+// import { useQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/_authenticated/x/y")({
-  component: () => <div>Hello /_authenticated/x/y!</div>,
+  component: YDisplay,
   validateSearch: asTypedParams<{ miro?: string }>(),
 });
+
+function YDisplay() {
+  // const dbg = useQuery({
+  //   queryKey: ["dbg", { x: 11, y: 12 }],
+  //   queryFn: async ({ queryKey, ...rest }) => {
+  //     console.log(">>>>> queryKey: ", queryKey, rest);
+  //     return new Promise((resolve) => setTimeout(resolve, 500, "miro"));
+  //   },
+  // });
+  // console.log(">>> dbg: ", JSON.parse(JSON.stringify(dbg)));
+
+  return <div>Hello /_authenticated/x/y!</div>;
+}
