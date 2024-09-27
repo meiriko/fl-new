1231f66d6a3a0e27a5dfeaada61eede5ee5af2df
Meir <meir@finaloop.com>
2024-09-27 15:24:31 +0300

    add dataApi lib with basic config

---
+++ libs/dataApi/eslint.config.js
@@ -0,0 +1,19 @@
+import tseslint from "typescript-eslint";
+import baseConfig, { mergeImportRestriction } from "../../eslint.config.js";
+
+export default tseslint.config(...baseConfig, {
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      mergeImportRestriction(baseConfig, {
+        patterns: [
+          {
+            group: ["@fl/*"],
+            message:
+              "@fl/dataApi is a low level library, it should not import other @fl/* libraries",
+          },
+        ],
+      }),
+    ],
+  },
+});
---
+++ libs/dataApi/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./lib/main.ts",
+      name: "@fl/routing",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ include: ["lib"] })],
+});
