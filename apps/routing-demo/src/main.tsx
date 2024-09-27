216d760646cd764bec5da8178db71b75126e9fc7
Meir <meir@finaloop.com>
2024-09-27 16:55:39 +0300

    added tanstack query dev tools and staleTime

--- apps/routing-demo/src/main.tsx
+++ apps/routing-demo/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createRouter({ routeTree, context: { demo: "miro" } });
 
@@ -11,9 +13,20 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * (60 * 1000), // 1 minute
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </StrictMode>,
 );
