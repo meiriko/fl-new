19e33993f9e1f1745bdfddde7e1d38eb94b87432
Meir <meir@finaloop.com>
2024-09-27 23:49:29 +0300

    sample utils usage

--- apps/routing-demo/src/routes/_authenticated/x/y/route.tsx
+++ apps/routing-demo/src/routes/_authenticated/x/y/route.tsx
@@ -10,12 +10,13 @@ export const Route = createFileRoute("/_authenticated/x/y")({
 function YDisplay() {
   // const dbg = useQuery({
   //   queryKey: ["dbg", { x: 11, y: 12 }],
-  //   queryFn: async ({ queryKey, ...rest }) => {
+  //   queryFn: async function ({ queryKey, ...rest }): Promise<{ age: number }> {
   //     console.log(">>>>> queryKey: ", queryKey, rest);
-  //     return new Promise((resolve) => setTimeout(resolve, 500, "miro"));
+  //     return new Promise((resolve) => setTimeout(resolve, 500, { age: 55 }));
   //   },
   // });
-  // console.log(">>> dbg: ", JSON.parse(JSON.stringify(dbg)));
-
+  // // console.log(">>> dbg: ", JSON.parse(JSON.stringify(dbg)));
+  //
+  // console.log(">>. ", dbg.data?.age);
   return <div>Hello /_authenticated/x/y!</div>;
 }
